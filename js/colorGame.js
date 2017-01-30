a44786c3c6a495c0ef62d136ad2092df84be890b
Sujanth <sujanth.sebamalaithasan@appearition.com>
2017-01-30 12:11:52 +1100

    Completed basic features

--- js/colorGame.js
+++ js/colorGame.js
@@ -1,15 +1,10 @@
-var colors = [
-  "rgb(255, 0, 0)",
-  "rgb(255, 255, 0)",
-  "rgb(0, 255, 0)",
-  "rgb(0, 255, 255)",
-  "rgb(0, 0, 255)",
-  "rgb(255, 0, 255)"
-]
+var colors = generateRandomColor(6);
 
 var squares = document.querySelectorAll(".square");
-var pickedColor = colors[3];
+var pickedColor = pickColor();
 var colorDisplay = document.getElementById("colorDisplay");
+var messageDisplay = document.querySelector("#message");
+var h1Color = document.querySelector("h1");
 
 colorDisplay.textContent = pickedColor;
 
@@ -23,10 +18,50 @@ for (var i = 0; i < squares.length; i++) {
     var clickedColor = this.style.background;
     //compare color to pickedColor
     if(clickedColor === pickedColor) {
-      alert("CORRECT");
+      messageDisplay.textContent = "Correct!";
+      changeColor(clickedColor);
+      h1Color.style.background = clickedColor;
     } else {
-      alert("WRONG")
+      this.style.background = "#232323";
+      messageDisplay.textContent = "Try Again";
     }
 
   });
+
+}
+
+function changeColor (color) {
+  //loop through all squares
+  for (var i = 0; i < squares.length; i++) {
+    //change square colors to given color
+    squares[i].style.background = color;
+  }
+}
+
+function pickColor() {
+  var random = Math.floor(Math.random() * colors.length);
+  return colors[random];
+}
+
+function generateRandomColor(num) {
+  //make an array
+  var arr = [];
+  //repaet num times
+  for (var i = 0; i < num; i++) {
+    //add num randam colors to array
+    arr.push(randomColor());
+  }
+  //return array
+  return arr;
+}
+
+function randomColor () {
+  //pick random red color
+  var red = Math.floor(Math.random() * 256);
+  //pick random green color
+  var green = Math.floor(Math.random() * 256);
+  //pick random blue color
+  var blue = Math.floor(Math.random() * 256);
+
+  return "rgb(" + red + ", " + green + ", " + blue + ")";
 }
